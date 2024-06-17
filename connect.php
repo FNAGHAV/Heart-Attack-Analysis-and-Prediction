40d7490074de90633480257fa7e9bee5e90f80ac
FNAGHAV <153868624+FNAGHAV@users.noreply.github.com>
2024-06-17 17:58:33 -0400

    latest Updated version - No prediction yet

---
+++ connect.php
@@ -0,0 +1,12 @@
+<?php
+$angina = $_POST['angia'];
+$bmi = $_POST['bmi'];
+$weight = $_POST['weight'];
+$height = $_POST['height'];
+$sleep = $_POST['sleep'];
+$physical = $_POST['physical'];
+$mental = $_POST['mental'];
+
+//Database Connection
+
+?>
